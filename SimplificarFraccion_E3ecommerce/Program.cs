7b9072a478d9ab79ec47da859c6725adbc1af37d
Juan Basualdo <frxxcko@gmail.com>
2021-02-15 02:31:25 -0300

    Challenge

--- SimplificarFraccion_E3ecommerce/Program.cs
+++ SimplificarFraccion_E3ecommerce/Program.cs
@@ -10,7 +10,7 @@ namespace SimplificarFraccion_E3ecommerce
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Desafío E3ecommerce Simplifación de Fracciones. Desarrollador: Juan Ignacio Basualdo");
+            Console.WriteLine("Desafío E3ecommerce Simplifación de Fracciones. Desarrollador: Juan Ignacio Basualdo.");
             Console.WriteLine("");
             Console.WriteLine("Por favor ingrese la fracción a simplificar. Por ejemplo: 5/15");
             string cadena = Console.ReadLine();
